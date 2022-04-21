db18504768bd03cabb2a4031e3520be06fda0d08
aumchayanut <aum31468@gmail.com>
2022-04-21 21:30:31 +0700

    SetHomeFinished
    PID almost
    UART I2C not yet

--- Module/Core/Src/main.c
+++ Module/Core/Src/main.c
@@ -48,38 +48,106 @@ TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+DMA_HandleTypeDef hdma_usart2_tx;
 
 /* USER CODE BEGIN PV */
-float x = 0 ;
-float y = 0 ;
-uint64_t t = 0 ;
-uint64_t _micros = 0;
-uint64_t TimestampEncoder = 0 ;
-uint64_t TimestampPWM = 0 ;
-uint64_t TimestampPID = 0 ;
-uint16_t PWMPercent = 0 ;
-uint16_t EndEffStatus = 0 ;
-float EncoderVel = 0 ;
-float VelocityRPM ;
-float Degree = 0 ;
-float StartPos,FinalPos ;
-uint8_t Direction = 0 ;
-uint8_t SetHomeBuffer[2] = {0} ;
-uint8_t ButtonBuffer[2] = {0} ;
-uint8_t SetHomeFlag = 0 ;
-uint8_t Set ;
-uint8_t StartSetHome = 0 ;
-static uint16_t Home,Now ;
 
-float request = 0 ;
-uint16_t PreviousPWM = 0 ;
-float preErr1,preErr2,Propotional,Integrator,Differentiator,preVel,P,I,D,Tau ;
-//float preErr1,preErr2,Propotional,Integrator,Differentiator,preVel ;
-//float P = 60 ;
-//float I = 1500 ;
-//float D = 30 ;
-//float Tau = 0.2 ;
-float SampleTime = 0.00001 ;
+//UART
+//typedef struct _UartStructure
+//{
+//	UART_HandleTypeDef *huart;
+//	uint16_t TxLen, RxLen;
+//	uint8_t *TxBuffer;
+//	uint16_t TxTail, TxHead;
+//	uint8_t *RxBuffer;
+//	uint16_t RxTail; //RXHeadUseDMA
+//
+//} UARTStucrture;
+//
+//UARTStucrture UART2 =
+//{ 0 };
+//
+//uint8_t MainMemory[255] =
+//{ 0 };
+//static uint8_t parameter[256] =
+//{ 0 };
+//
+//typedef enum
+//{
+//	DNMXP_idle,
+//	DNMXP_1stHeader,
+//	DNMXP_2ndHeader,
+//	DNMXP_3rdHeader,
+//	DNMXP_Reserved,
+//	DNMXP_ID,
+//	DNMXP_LEN1,
+//	DNMXP_LEN2,
+//	DNMXP_Inst,
+//	DNMXP_ParameterCollect,
+//	DNMXP_CRCAndExecute
+//
+//} UARTState;
+
+
+//PID
+int InitialPWM = 1300 ; //offset start pwm
+uint64_t TimestampPID = 0 ; //PID period
+float SampleTime = 0.00001 ; //PID period
+float PreviousPWM = 0 ;
+float preErr1,preErr2,Propotional,Integrator,Differentiator,preVel,Tau ;
+float DeltaU;
+float P = 20;
+float I = 5;
+float D = 0;
+float p,i,d;
+
+//Traj
+float StartTime ; //Start moving time
+uint8_t StartMoving = 0 ; //Start moving signal
+uint64_t t = 0 ; //traj gen period
+uint8_t ST = 0 ; //1 when gen traj
+float velocity; //velocity gen
+float position;
+float acceleration;
+float StartPos;
+float FinalPos = 30 ; //Degree
+float Qi,Qf,TA,TV,T,tau ;
+float TA1,TA2,TA3,TV1 ;
+uint64_t Trajtimestamp;
+uint8_t via_point = 0;
+
+
+//Set Home
+uint8_t HomeSignal[8] = {0} ; //read from encoder
+uint8_t SetHomeFlag = 0 ; //Status set home - 1 working
+uint8_t StartSetHome = 0 ; //Set home trigger
+uint8_t sum;
+uint64_t SetHomeTimeStamp = 0;
+
+
+//Read Encoder
+uint64_t TimestampEncoder = 0 ;//period of low pass read velocity
+float EncoderVel = 0 ; //Velovity after low pass
+
+
+//General
+float request = 0 ; //Velocity want to be
+uint16_t PWMPercent = 0 ; //Motor PWM 0 - 10000
+uint64_t _micros = 0;
+float VelocityRPM ; //velocity RPM unit
+float Degree = 0 ; //Position Degree unit
+uint8_t Direction = 0 ; //0 CW - 1 CCW - 2 Stop
+uint8_t ButtonBuffer[2] = {0} ; //Blue button
+
+
+float x = 0 ; // request changed
+
+uint16_t EndEffStatus = 0 ;
+uint8_t test = 0;
+
+
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -89,6 +157,7 @@ static void MX_USART2_UART_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_DMA_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 uint64_t micros() ;
@@ -97,10 +166,10 @@ void PWMgeneration() ;
 float Velocity() ;
 void PID() ;
 void PIDinit() ;
-void Trajectory();
+void FirstTraj();
 void SetHome() ;
 void EndEffWrite() ;
-void EndEffRead() ;
+void GoToGoal(float goal);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -140,8 +209,16 @@ int main(void)
   MX_TIM1_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
+  MX_DMA_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+
+//	UART2.huart = &huart2;
+//	UART2.RxLen = 255;
+//	UART2.TxLen = 255;
+//	UARTInit(&UART2);
+//	UARTResetStart(&UART2);
+
 
   PIDinit() ;
 
@@ -162,42 +239,75 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  EndEffRead() ;
-	  VelocityRPM = Velocity() ;
-	  Degree = htim3.Instance->CNT * 360.0 / 2048.0 ;
-	  PWMgeneration() ;
-//	  Set = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0) ;
-	  ButtonBuffer[0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) ;
-	  if (ButtonBuffer[0] && !ButtonBuffer[1])
+
+//***********General********************************************************************
+
+	  VelocityRPM = Velocity() ; //rpm unit
+	  Degree = htim3.Instance->CNT * 360.0 / 2048.0 ; //Degree unit
+	  PWMgeneration() ; //Gen PWM
+	  ButtonBuffer[0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) ; // Read Blue button
+//**************************************************************************************
+//**********Blue Button Push*********************
+	  if (!ButtonBuffer[0] && ButtonBuffer[1])
 	  {
-		  EndEffWrite() ;
-		  y = y+1 ;
-//		  StartSetHome = 1 ;
+//		  EndEffWrite() ;
+//		  z = 1 ;
+		  StartSetHome = 1 ; //Set home trigger
+		  SetHomeFlag = 0;
 //		  SetHomeFlag = 0 ;
 	  }
-//	  if (StartSetHome == 1)
-//	  {
-//		  SetHome() ;
-//	  }
 	  ButtonBuffer[1] = ButtonBuffer[0] ;
+//************************************************
+//**********Set Home******************************
+	  if (StartSetHome == 1)
+	  {
+		  SetHome() ;
+	  }
+//***********************************************
+//**************PID******************************
 	  if (micros() - TimestampPID > 1000)
 	  {
-		  if (request != 0)
+		  if (StartMoving || StartSetHome)
 		  {
+			  P = p;
+			  I = i;
+			  D = d;
 			  PID() ;
 		  }
 		  TimestampPID = micros() ;
 	  }
+//************************************************
+
+
+
+//********other**************
 	  if (request == 0)
 	  {
 		  Direction = 2 ;
 	  }
-	  if (x != request)
+//****************************
+
+
+//	  if (x != request) //Change request
+//	  {
+//		  PIDinit() ;
+//		  StartMoving = 1 ;
+//	  }
+//
+//	  x = request ; //save previous request
+
+	  if (StartMoving == 0)
 	  {
-		  PIDinit() ;
+		  T = 0;
+		  TV = 0;
+		  TA = 0;
+		  ST = 0;
 	  }
-	  x = request ;
-//	  Trajectory() ;
+	  if (StartMoving == 1)
+	  {
+		  FirstTraj();
+	  }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -265,7 +375,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.ClockSpeed = 1000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -341,10 +451,6 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
   }
@@ -476,10 +582,10 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 512000;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Parity = UART_PARITY_EVEN;
   huart2.Init.Mode = UART_MODE_TX_RX;
   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
@@ -490,6 +596,25 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+  /* DMA1_Stream6_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
 
 }
 
@@ -533,82 +658,138 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void EndEffRead()
-{
-	HAL_I2C_Master_Receive_IT(&hi2c1, End_Eff, EndEffStatus, 1) ;
-}
 void EndEffWrite()
 {
-	if (HAL_I2C_STATE_READY)
+	if (hi2c1.State == HAL_I2C_STATE_READY)
 	{
-		HAL_I2C_Master_Transmit_IT(&hi2c1, End_Eff, 0x45, 1) ;
-		HAL_I2C_Master_Transmit_IT(&hi2c1, End_Eff, 0x23, 1) ;
-//		HAL_I2C_Master_Transmit_IT(&hi2c1, End_Eff, 0x56, 2) ;
+//		HAL_Delay(500);
+//		HAL_I2C_Master_Transmit(&hi2c1, 0x23 << 1, 0x45, 1	, 1000); //Write eff
+		HAL_I2C_Master_Transmit_IT(&hi2c1, 0x23<<1, 0x45, 1);
 	}
+
+}
+void GoToGoal(float goal)
+{
 
 }
 void SetHome()
 {
+	HomeSignal[0] = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0) ; //Read set home
+	sum = HomeSignal[0] + HomeSignal[1]+ HomeSignal[2]+ HomeSignal[3]+ HomeSignal[4]+ HomeSignal[5]+ HomeSignal[6]+ HomeSignal[7];
+
 	if (SetHomeFlag == 0)
 	{
-		Direction = 0 ;
-		request = 5 ;
+		request = 5;
+		if (sum > 0)
+		{
+			SetHomeFlag = 1;
+			SetHomeTimeStamp = micros();
+		}
 	}
-	Now = htim3.Instance->CNT ;
-	if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0))
-	{
-		Home = htim3.Instance->CNT ;
-		SetHomeFlag = 1 ;
-	}
-
 	if (SetHomeFlag == 1)
 	{
-		if (Now > Home)
+		request = 0;
+		if (micros()-SetHomeTimeStamp > 1000000)
 		{
-			Direction = 0 ;
-			request = 1 ;
-		}
-		if (Now < Home)
-		{
-			Direction = 1 ;
-			request = 1 ;
-		}
-		if (Home == Now)
-		{
-			Direction = 2 ;
-			request = 0 ;
-			StartSetHome = 0 ;
+			SetHomeFlag = 2;
 		}
 	}
+	if (SetHomeFlag == 2)
+	{
+		request = -0.5;
+		if (sum > 0)
+		{
+			request = 0;
+			SetHomeFlag = 3;
+		}
+	}
+
 
 
 }
-//void Trajectory()
-//{
-//	StartPos = Degree*3.14159/180 ;
-//	float Qi = StartPos ;
-//	float Qf = FinalPos ;
-//	float DeltaTime ;
-//	float a0 = Qi ;
-//	float a1 = 0 ;
-//	float a2 = 0 ;
-//	float a3 = (1/(2*DeltaTime^3))*(20(Qf-Qi)) ;
-//	float a4 = (1/(2*DeltaTime^4))*(30(Qi-Qf)) ;
-//	float a5 = (1/(2*DeltaTime^5))*(12(Qf-Qi)) ;
-//	float StartTime ;
-//	uint8_t ST = 0 ;
-//	if (micros() - t > 1000)
-//	{
-//		if (ST == 0)
-//		{
-//			StartTime = micros() ;
-//			ST = 1 ;
-//		}
-//		float t = micros() - StartTime ;
-//		request = t + (2*a2*t) + (3*a3*t^2) + (4*a4*t^3) + (5*a5*t^4) ;
-//		t = micros() ;
-//	}
-//}
+
+
+void FirstTraj()
+{
+	float Vmax = 10 ; //rpm
+	Vmax = Vmax * 0.10472 ; //rad per sec
+	float Amax = 0.5 ;  //rad per sec square
+	if (ST == 0)
+	{
+		StartTime = micros() ;
+		ST = 1 ;
+		StartPos = Degree * 3.14159 / 180.0 ; //rad
+	}
+	tau = (micros() - StartTime) / 1000000 ; //sec
+
+	Qi = StartPos ;
+	Qf = FinalPos * 3.14159 / 180.0 ;
+
+	if (Qf - Qi > 3.14159265)
+	{
+		Qi += 2*3.14159265;
+	}
+	if (Qf - Qi < -3.14159265)
+	{
+		Qf += 2*3.14159265;
+	}
+	TA1= (40.0 * sqrt(3.0))*(Qf - Qi);
+	TA2 = TA1/(3.0*Amax) ;
+	if (TA2 >= 0)
+	{
+		TA3 = TA2;
+	}
+	else
+	{
+		TA3 = TA2 - (2*TA2);
+	}
+	TA = sqrt(TA3) / 2.0 ;
+
+	TV1 = (15*Qf - 15*Qi)/(8*Vmax);
+	if (TV1 >= 0)
+	{
+		TV = TV1;
+	}
+	else
+	{
+		TV = TV1 - (2*TV1);
+	}
+
+	if (TV > TA)
+	{
+		T = TV ;
+	}
+	if (TV <= TA)
+	{
+		T = TA ;
+	}
+	float a0 = Qi ;
+	float a1 = 0 ;
+	float a2 = 0 ;
+	float a3 = (1 / (2 * (T*T*T))) * (20 * (Qf-Qi)) ;
+	float a4 = (1 / (2 * (T*T*T*T))) * (30 * (Qi-Qf)) ;
+	float a5 = (1 / (2 * (T*T*T*T*T))) * (12 * (Qf-Qi)) ;
+
+	if (micros() - StartTime < T*1000000)
+	{
+		if (micros() - Trajtimestamp > 1500)
+		{
+			position = a0 + a1*tau + a2*tau*tau + a3*tau*tau*tau + a4*tau*tau*tau*tau + a5*tau*tau*tau*tau*tau; //rad
+			velocity = a1 + 2*a2*tau + 3*a3*tau*tau + 4*a4*tau*tau*tau + 5*a5*tau*tau*tau*tau; //rad/s
+			acceleration = 2*a2 + 6*a3*tau + 12*a4*tau*tau + 20*a5*tau*tau*tau; //rad per secsquare
+
+			position = position * 180 / 3.1415 ; //degree
+			velocity = velocity * 9.549297; //rpm
+			Trajtimestamp = micros() ;
+			request = velocity ;
+		}
+	}
+	if (micros() - StartTime > (T*1000000)+500000)
+	{
+		StartMoving = 0;
+	}
+
+}
 
 void PIDinit()
 {
@@ -627,10 +808,6 @@ void PIDinit()
 
 void PID()
 {
-	P = 60 ;
-	I = 1500 ;
-	D = 30 ;
-	Tau = 0.2 ;
 	float req,Vel ;
 	if (request < 0)
 	{
@@ -644,52 +821,88 @@ void PID()
 		Vel = VelocityRPM ;
 		Direction = 0 ;
 	}
-	float error = req - Vel ;
-	Propotional = P * error ;
-	Integrator = Integrator + (0.5 * I * SampleTime * (error + preErr1)) ;
-
-	//********Anti Windup*************
-	float maxInt,minInt ;
-	if (Propotional < 10000)
-	{
-		maxInt = 10000 - Propotional ;
-	}
-	else
-	{
-		maxInt = 0 ;
-	}
-	if (Propotional > 0)
-	{
-		minInt = 0 - Propotional ;
-	}
-	else
-	{
-		minInt = 0 ;
-	}
-	//********************************
-	if (Integrator > maxInt)
-	{
-		Integrator = maxInt ;
-	}
-	else if (Integrator < minInt)
-	{
-		Integrator = minInt ;
-	}
-
-	Differentiator = (2*D*(VelocityRPM - preVel)) + (Differentiator * (2*Tau - SampleTime)) / (2 * Tau + SampleTime) ;
-
-	PWMPercent = Propotional + Integrator + Differentiator + 5500;
+	float error = req - Vel;
+	DeltaU = ((P+I+D)*error) - ((P+(2*D))*preErr1) + (D*preErr2) ;
+	PWMPercent = PreviousPWM + DeltaU + InitialPWM ;
 	if (PWMPercent > 10000)
 	{
-		PWMPercent = 10000 ;
+		PWMPercent = 10000;
 	}
 	if (PWMPercent < 0)
 	{
 		PWMPercent = 0 ;
 	}
+	PreviousPWM = PWMPercent - InitialPWM ;
+	preErr2 = preErr1 ;
 	preErr1 = error ;
-	preVel = Vel ;
+
 }
+
+//void PID()
+//{
+////	P = 60 ;
+////	I = 1500 ;
+////	D = 30 ;
+//	Tau = 0.2 ;
+//	float req,Vel ;
+//	if (request < 0)
+//	{
+//		req = - request ;
+//		Vel = - VelocityRPM ;
+//		Direction = 1 ;
+//	}
+//	if (request > 0)
+//	{
+//		req = request ;
+//		Vel = VelocityRPM ;
+//		Direction = 0 ;
+//	}
+//	float error = req - Vel ;
+//	Propotional = P * error ;
+//	Integrator = Integrator + (0.5 * I * SampleTime * (error + preErr1)) ;
+//
+//	//********Anti Windup*************
+//	float maxInt,minInt ;
+//	if (Propotional < 10000)
+//	{
+//		maxInt = 10000 - Propotional ;
+//	}
+//	else
+//	{
+//		maxInt = 0 ;
+//	}
+//	if (Propotional > 0)
+//	{
+//		minInt = 0 - Propotional ;
+//	}
+//	else
+//	{
+//		minInt = 0 ;
+//	}
+//	//********************************
+//	if (Integrator > maxInt)
+//	{
+//		Integrator = maxInt ;
+//	}
+//	else if (Integrator < minInt)
+//	{
+//		Integrator = minInt ;
+//	}
+//
+//	Differentiator = (2*D*(VelocityRPM - preVel)) + (Differentiator * (2*Tau - SampleTime)) / (2 * Tau + SampleTime) ;
+//
+//	PWMPercent = Propotional + Integrator + Differentiator + InitialPWM;
+//	if (PWMPercent > 10000)
+//	{
+//		PWMPercent = 10000 ;
+//	}
+//	if (PWMPercent < 0)
+//	{
+//		PWMPercent = 0 ;
+//	}
+//	preErr1 = error ;
+//	preVel = Vel ;
+//}
 
 
 float Velocity()
@@ -699,28 +912,24 @@ float Velocity()
 		  TimestampEncoder = micros();
 		  EncoderVel = (EncoderVel * 999 + EncoderVelocity_Update()) / 1000.0;
 	  }
-	  return EncoderVel * 60.0 / 2048.0;
+	  return EncoderVel * 60.0 / 2048.0; //pulse per sec to rpm
 }
 void PWMgeneration()
 {
 	  if (Direction == 0)
 	  {
 		  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 10000);
-		  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
 	  }
 	  if (Direction == 1)
 	  {
 		  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
-		  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 10000);
 	  }
 	  if (Direction == 2)
 	  {
 		  PIDinit() ;
 		  PWMPercent = 0 ;
-		  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
-		  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
 	  }
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, PWMPercent);
+	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, PWMPercent);
 }
 
 #define  HTIM_ENCODER htim3
